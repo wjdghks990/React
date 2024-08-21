dbb6a515e4bb165c0ce3e322424f86f104fa14a4
wjdghks990 <qnfxksmstkwk@naver.com>
2024-08-21 13:18:11 +0900

    메뉴 예제 적용

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from "./logo.svg";
+import { Link, Route, Routes } from "react-router-dom";
+import "./App.css";
+import "./css/menu.css";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
 
 function App() {
+  // const title = "Global IT";
+  // const title_style = { color: "#f00", fontSize: "30px" };
+
   return (
     // JSX (html과 매우 유사한 형식)
-    <div className="App">  
+    <div className="App">
+      {/* 메뉴 */}
+      <div id="header">
+        <ul id="nav">
+          {/* <li id="apple">&nbsp;</li> */}
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/store">Store</Link>
+          </li>
+          <li>Mac</li>
+          <li>iPad</li>
+          <li>iPhone</li>
+          <li>Watch</li>
+          <li>AirPods</li>
+          <li>TV&amp;Home</li>
+          <li>Only on Apple</li>
+          <li>Accessories</li>
+          <li>Support</li>
+        </ul>
+      </div>
 
-      <p>여기가 메인인가?</p>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+      </Routes>
+
+      {/* 본문 JSX에서 변수 사용 및 스타일 주기 위아래 문장 동일*/}
+      {/* <h2 style={{ color: "#f00", fontSize: "30px" }}>{title}</h2>
+      <h2 style={title_style}>{title}</h2> */}
     </div>
   );
 }
---
+++ src/pages/Store.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Store() {
+  return <div>Store화면입니다.</div>;
+}
+
+// 외부에서 사용할 수 있게끔 해줌
+export default Store;
